ef702938f6424552d2de5949d4ac49ae4a9967c7
David Alvarez <daveyalvarez03@gmail.com>
2020-09-04 16:52:40 -0700

    Updates

---
+++ precache-manifest.49d8bfa387c34c8cc7562dd1050922fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b96eede2c5d91d5a159060d95d19e11",
+    "url": "/dnd-encounter/index.html"
+  },
+  {
+    "revision": "48ae7f7c7647618c2cf9",
+    "url": "/dnd-encounter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1950b31b1cce7702e42a",
+    "url": "/dnd-encounter/static/js/2.bab5d0db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dnd-encounter/static/js/2.bab5d0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ae7f7c7647618c2cf9",
+    "url": "/dnd-encounter/static/js/main.b05bbc50.chunk.js"
+  },
+  {
+    "revision": "3042a5759422443a3992",
+    "url": "/dnd-encounter/static/js/runtime-main.7f83624e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dnd-encounter/static/media/logo.5d5d9eef.svg"
+  }
+]);
