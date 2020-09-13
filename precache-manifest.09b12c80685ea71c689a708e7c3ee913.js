b680d4bed8d39c9f5941a96a423b433892f4c26d
David Alvarez <daveyalvarez03@gmail.com>
2020-09-12 23:53:53 -0700

    Updates

--- precache-manifest.09b12c80685ea71c689a708e7c3ee913.js
+++ precache-manifest.09b12c80685ea71c689a708e7c3ee913.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c8797058d8d868e1f770fe9da7bd7220",
+    "revision": "4ab5cc3e3dff7f91ecd71a2f6eafab79",
     "url": "/dnd-encounter/index.html"
   },
   {
-    "revision": "40a13bfca4f44c936a59",
+    "revision": "be183b0e504d043b6f23",
     "url": "/dnd-encounter/static/css/main.27ca3aff.chunk.css"
   },
   {
-    "revision": "86c13b32bdc72afe4cf1",
-    "url": "/dnd-encounter/static/js/2.606da6a1.chunk.js"
+    "revision": "32fe4afbea3cc4bd748f",
+    "url": "/dnd-encounter/static/js/2.2a0d88d7.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/dnd-encounter/static/js/2.606da6a1.chunk.js.LICENSE.txt"
+    "url": "/dnd-encounter/static/js/2.2a0d88d7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "40a13bfca4f44c936a59",
-    "url": "/dnd-encounter/static/js/main.098db3c8.chunk.js"
+    "revision": "be183b0e504d043b6f23",
+    "url": "/dnd-encounter/static/js/main.4337a7ba.chunk.js"
   },
   {
     "revision": "3042a5759422443a3992",
